43da931c34d8acfc61a0d047876108a10c9570fc
Niels Abildgaard <niels.abildgaard@gmail.com>
2017-05-08 17:01:41 +0200

    scala code that works

---
+++ scala/relation.scala
@@ -0,0 +1,29 @@
+import scala.language.implicitConversions
+
+sealed trait RelationBool[+A] {
+  def evaluate[B >: A, C >: B] (y: RelationBool[B], comparisonEvaluator: Int=>Boolean, ordering: math.Ordering[C]) : RelationBool[B] = (this, y) match {
+    case (PossiblyTrue(xl, xr), PossiblyTrue(yl,yr)) if comparisonEvaluator(ordering.compare(xr,yl)) => PossiblyTrue(xl, yr)
+    case (True, v) => v
+    case (v, True) => v
+    case _ => False
+  }
+
+  def *<* [B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ <  0, ordering)
+  def *<=*[B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ <= 0, ordering)
+  def *>* [B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ >  0, ordering)
+  def *>=*[B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ >= 0, ordering)
+  def *==*[B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ == 0, ordering)
+  def *!=*[B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ != 0, ordering)
+  def *<>*[B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ != 0, ordering)
+}
+case object False extends RelationBool[Nothing]
+case class PossiblyTrue[A](leftmost: A, rightmost: A) extends RelationBool[A]
+case object True extends RelationBool[Nothing]
+
+def PossiblyTrue1[A] = (value: A) => PossiblyTrue(value, value)
+
+implicit def valueToRelationBool[A] = (value: A) => PossiblyTrue1(value)
+implicit def relationBoolToBoolean[A] (x: RelationBool[A]): Boolean = x match {
+  case False => false
+  case _ => true
+}
