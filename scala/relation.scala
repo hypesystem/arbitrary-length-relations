e618c1638c6d642f90c62b19c6cdec02682dfa98
Bladtman <sigurt.bladt@gmail.com>
2017-08-13 18:55:50 +0200

    Made a version of RelationBool where only one value if "traced" along. This is possible because I know the *<* operation is left associative, (scala ops whose name does _not_ end with a colon (':') are left-assoc). Hence we can just trace the right-most value along.

--- scala/relation.scala
+++ scala/relation.scala
@@ -2,7 +2,7 @@ import scala.language.implicitConversions
 
 sealed trait RelationBool[+A] {
   def evaluate[B >: A, C >: B] (y: RelationBool[B], comparisonEvaluator: Int=>Boolean, ordering: math.Ordering[C]) : RelationBool[B] = (this, y) match {
-    case (PossiblyTrue(xl, xr), PossiblyTrue(yl,yr)) if comparisonEvaluator(ordering.compare(xr,yl)) => PossiblyTrue(xl, yr)
+    case (PossiblyTrue(x), PossiblyTrue(y)) if comparisonEvaluator(ordering.compare(x,y)) => PossiblyTrue(y)
     case (True, v) => v
     case (v, True) => v
     case _ => False
@@ -17,12 +17,11 @@ sealed trait RelationBool[+A] {
   def *<>*[B >: A, C >: B] (y: RelationBool[B]) (implicit ordering: math.Ordering[C]) = this.evaluate(y, _ != 0, ordering)
 }
 case object False extends RelationBool[Nothing]
-case class PossiblyTrue[A](leftmost: A, rightmost: A) extends RelationBool[A]
+case class PossiblyTrue[A](value: A) extends RelationBool[A]
 case object True extends RelationBool[Nothing]
 
 object RelationBool {
-  def PossiblyTrue1[A] = (value: A) => PossiblyTrue(value, value)
-  implicit def valueToRelationBool[A] = (value: A) => PossiblyTrue1(value)
+  implicit def valueToRelationBool[A] = (value: A) => PossiblyTrue(value)
   implicit def relationBoolToBoolean[A] (x: RelationBool[A]): Boolean = x match {
     case False => false
     case _ => true
